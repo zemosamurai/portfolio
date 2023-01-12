1c5bf373df5f8e1645949ecefa839f20a020dfbf
zemosamurai <zemosamurai@gmail.com>
2023-01-12 12:46:34 +0300

    add style

--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import s from './Main.module.css'
+import s from './Main.module.scss'
 import styleContainer from '../../common/styles/Container.module.css'
 
 export const Main = () => {
@@ -7,9 +7,9 @@ export const Main = () => {
         <div className={s.mainBlock}>
             <div className={styleContainer.container}>
                 <div className={s.text}>
-                    <h1>Kim Harokhau</h1>
-                    <p>Frontend Developer based In Belarus</p>
-                    <button>Get Connected</button>
+                    <h1 className={s.name}>Kim <span>Harokhau</span></h1>
+                    <p className={s.description}>Frontend Developer based In Belarus</p>
+                    <button className={s.btn}>Get Connected</button>
                 </div>
                 <div className={s.photo}>
                 </div>
