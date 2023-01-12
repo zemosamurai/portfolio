9861c858440a2f5a408a533408e06496761b0dd4
zemosamurai <zemosamurai@gmail.com>
2023-01-12 12:46:22 +0300

    refactore code

--- src/Components/Contact/Contact.tsx
+++ src/Components/Contact/Contact.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import s from './Contact.module.css'
 import styleContainer from '../../common/styles/Container.module.css'
+import {Title} from "../../common/commonComponents/Title/Title";
 
 export const Contact = () => {
     return (
         <div className={s.contactBlock}>
             <div className={`${styleContainer.container} ${s.container}`}>
-                <div className={s.contactBlockTitle}>
-                    <span>Contact</span>
-                    <h2>
-                        I Want to Hear from You
-                    </h2>
-                </div>
+                <Title title={'Contact'} text={'I Want to Hear from You'}/>
                 <form className={s.formBlock} name="email_form">
                     <input placeholder="Your Name" type="text" name="first_name"/>
                     <input placeholder="Your E-mail" type="email" name="email"/>
