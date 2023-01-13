6bd8f7b3dc8db70e39de3d9f75eee357b2c275e4
zemosamurai <zemosamurai@gmail.com>
2023-01-13 12:23:21 +0300

    new Props and map(props.projectsArr)

--- src/Components/Projects/Projects.tsx
+++ src/Components/Projects/Projects.tsx
@@ -1,19 +1,36 @@
 import React from 'react';
-import s from './Projects.module.css'
+import s from './Projects.module.scss'
 import styleContainer from '../../common/styles/Container.module.css'
-import {Project} from "./Project/Project";
+import {Project} from './Project/Project';
+import {Title} from '../../common/commonComponents/Title/Title';
 
-export const Projects = () => {
+export type ProjectType = {
+    id?: string
+    title: string
+    description: string
+    img: {backgroundImage: string}
+}
+type ProjectsPropsType = {
+    projectsArr: Array<ProjectType>
+}
+
+export const Projects = ({projectsArr}: ProjectsPropsType) => {
+    const titleFromTitle = 'Projects'
+    const textFromTitle = 'Projects I have Worked On'
+
     return (
         <div className={s.projectsBlock}>
             <div className={`${styleContainer.container} ${s.container}`}>
-                <div className={s.aboutBlockTitle}>
-                    <span>Projects</span>
-                    <h2>Projects I have Worked On</h2>
-                </div>
+                <Title title={titleFromTitle} text={textFromTitle}/>
                 <div className={s.projects}>
-                    <Project/>
-                    <Project/>
+                    {projectsArr.map(el => {
+                        return <Project
+                            style={el.img}
+                            key={el.id}
+                            title={el.title}
+                            description={el.description}
+                        />
+                    })}
                 </div>
             </div>
         </div>
